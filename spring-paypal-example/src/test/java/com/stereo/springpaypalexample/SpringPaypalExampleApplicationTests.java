f2ab8a1322d4e06662a58058b0cfeeedcfc76108
Pareek, Rahul <rahul_pareek@optum.com>
2022-03-18 20:21:57 +0530

    Initial commit

---
+++ spring-paypal-example/src/test/java/com/stereo/springpaypalexample/SpringPaypalExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stereo.springpaypalexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPaypalExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
